d207993aa5ea76a97226ada6f1d011d7b4d8efce
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-20 10:42:41 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    cin >> a >> b >> c;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+    return 0;
+}
