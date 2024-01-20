43ef67fab432ae57629d39f868b27e2b45ffb0b3
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-20 15:27:50 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240120/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int num;
+        cin >> num;
+        if(num%3==0){
+            cout << num << endl;
+        }
+    }
+    return 0;
+}
