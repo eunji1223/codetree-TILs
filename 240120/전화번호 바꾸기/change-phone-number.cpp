4a7fec641256ed9ba6fddad493d61fc7b53c436b
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-20 11:59:17 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int front, middle, behind;
+    char n;
+
+    cin >> front >> n >> middle >> n >> behind;
+
+    cout << "0" << front << "-" << behind << "-" << middle;    
+    return 0;
+}
