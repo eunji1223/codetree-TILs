e579d00c7e37e72ade3b6efc0c869c960a517a1f
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-20 13:00:38 +0900

    Title: 홀수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240120/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    for(int i=a; i<b+1; i++){
+        if(i%2==0){
+            continue;
+        }
+        else{
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
