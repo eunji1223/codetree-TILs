853871513b6c92ae0dabf51517b6dcb1664836b6
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-25 17:38:28 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/요일 맞추기/guess-day-of-week.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+int returndays(int m, int d){
+    int days = 0;
+    for(int i=0; i<m; i++){
+        days += num_of_days[i];
+    }
+    return days + d;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    int day1 = returndays(m1, d1);
+    int day2 = returndays(m2, d2);
+
+    int result; 
+    if((day2-day1)<0){
+        result = day1-day2+5;
+    }
+    else{
+        result = day2-day1;
+    }
+    cout << day[result%7];
+    
+    return 0;
+}
