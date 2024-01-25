97416cc0c9e9af5d03ae34f769c0ad5117ab7e27
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-01-25 16:32:01 +0900

    Title: 반복 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printn(int n){
+    for(int i=0; i<n; i++){
+        cout << "12345^&*()_" << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    printn(n);
+    return 0;
+}
