713b58e31e93f17a18bac2aa64d5ce5c4c76a383
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-02-03 02:56:15 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240203/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, r, c;
+    int dx[] = {-1, 1, 0, 0};
+    int dy[] = {0, 0, -1, 1};
+    cin >> n >> r >> c;
+
+    r-=1;
+    c-=1;
+
+    int grid[n][n];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> grid[i][j];
+        }
+    }
+
+    cout << grid[r][c] << " ";
+
+    bool iscompleted = false;
+    int curr_r, curr_c;
+    curr_r = r;
+    curr_c = c;
+    while(iscompleted == false){
+        int max, curr;
+        max = grid[curr_r][curr_c];
+        curr = max;
+        for(int i=0; i<4; i++){
+            if(grid[curr_r+dx[i]][curr_c+dy[i]] > max){
+                max = grid[curr_r+dx[i]][curr_c+dy[i]];
+                curr_r = curr_r + dx[i];
+                curr_c = curr_c + dy[i];
+                max = grid[curr_r][curr_c];
+                break;
+            }
+        }
+        if(curr == max){
+            iscompleted = true;
+        }
+        else{
+            cout << max << " ";
+        }
+    }
+
+    
+    return 0;
+}
