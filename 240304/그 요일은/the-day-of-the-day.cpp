af11198ab8a5b42abf0e33e147891e51ab704406
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-04 23:12:44 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240304/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+int returndays(int m, int d){
+    int days = 0;
+    for(int i=0; i<m; i++){
+        days += num_of_days[i];
+    }
+    return days + d;
+}
+
+int main() {
+    int m1, d1, m2, d2;
+    string inputday;
+    int result = 0;
+
+    cin >> m1 >> d1 >> m2 >> d2 >> inputday;
+    
+    int day1 = returndays(m1, d1);
+    int day2 = returndays(m2, d2);
+    
+    int inputidx;
+    for(int i=0; i<7; i++){
+        if(inputday == day[i]){
+            inputidx = i;
+            break;
+        }
+    }
+    if((day1%7-1) < inputidx){
+        result += 1;
+    }
+    if(inputidx < (day2%7-1)){
+        result += 1;
+    }
+
+    result += (day2-day1)/7;
+
+    cout << result;
+    
+
+    return 0;
+}
