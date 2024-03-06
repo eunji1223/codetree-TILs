8b869145ca454cea13fc48bf251af62baa751dca
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-06 22:37:51 +0900

    Title: 최대공약수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240306/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int n, int m){
+    int min;
+    int max = 0;
+
+    if(n>m){
+        min = m;
+    }
+    else{
+        min = n;
+    }
+    for(int i=2; i<=min; i++){
+        if(n%i==0 && m%i==0){
+            max = i;
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << GCD(n, m);
+}
