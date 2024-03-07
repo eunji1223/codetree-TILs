0c90b3ee056e80210bfc05a4b5c7d0bb29633f96
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-07 15:20:22 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240307/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int count(int a, int b){
+    int cnt = 0;
+    for(int i=a; i<=b; i++){
+        if(i%3==0){
+            cnt++;
+            continue;
+        }
+        int num = i;
+        while(true){
+            if(num<10){
+                if(num%3==0){
+                    cnt++;
+                }
+                break;
+            }
+            if((num%10)%3==0){
+                cnt++;
+                break;
+            }
+            num/=10;
+        }
+    }
+    return cnt;
+}
+
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << count(a, b);
+    return 0;
+}
