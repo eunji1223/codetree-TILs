8aafb05ae61122e911ef8e1bf37c0b1a828952da
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-11 23:11:46 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240311/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240311/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 bool isPrime(int n){
+    if(n==1){
+        return false;
+    }
     for(int i=2; i<n; i++){
         if(n%i==0){
             return false;
@@ -16,7 +19,7 @@ int main() {
 
     cin >> a >> b;
 
-    for(int i=a; i<=b; i++){
+    for(int i=a; i<b+1; i++){
         if(isPrime(i)){
             sum+=i;
         }
