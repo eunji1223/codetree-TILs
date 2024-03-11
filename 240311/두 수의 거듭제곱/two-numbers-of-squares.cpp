e33de76d89a7320229b2a27cc29a7fe5ddca4844
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-03-11 23:47:08 +0900

    Title: 두 수의 거듭제곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240311/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int calculator(int a, int b){
+    int res=1;
+    for(int i=0; i<b; i++){
+        res *= sum;
+    }
+    return res;
+}
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << calculator(a, b);
+    return 0;
+}
