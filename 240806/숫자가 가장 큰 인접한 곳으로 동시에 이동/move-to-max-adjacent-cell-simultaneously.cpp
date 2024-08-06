92014d22de1be6f02b64971a38d3d8dfb60b8e8f
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-08-06 16:22:06 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+#define MaxGrid 20
+
+int arr[MaxGrid][MaxGrid];
+int arr_simulate[MaxGrid][MaxGrid] = {0, };
+
+int dx[4] = { -1, 1, 0, 0};
+int dy[4] = { 0, 0, -1, 1};
+
+void Simulation(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr_simulate[i][j] == 1){
+                int max = arr[i][j];
+                for(int k=0; k<4; k++){
+                    int row = i+dx[k];
+                    int col = j+dy[k];
+                    if(arr[row][col] > max){
+                        max = arr[row][col];
+                        arr_simulate[i][j] = 0;
+                        arr_simulate[row][col] += 1;
+                        if(arr_simulate[row][col] > 1){
+                            arr_simulate[row][col] = 0;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m, t;
+    cin >> n >> m >> t;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        int row, col;
+        cin >> row >> col;
+        row--;
+        col--;
+        arr_simulate[row][col] = 1;
+    }
+
+    for(int i=0; i<t; i++){
+        Simulation(n);
+    }
+
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(arr_simulate[i][j] == 1)
+                cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
