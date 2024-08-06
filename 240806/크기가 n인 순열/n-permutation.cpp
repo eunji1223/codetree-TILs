6252a938e8d4436f12dad4f705ea480bb9446052
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-08-06 23:19:28 +0900

    Title: 크기가 n인 순열, Time: 112ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/크기가 n인 순열/n-permutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+#define MaxArraySize 8
+
+int n;
+int arr[MaxArraySize];
+bool visited[MaxArraySize] = { false, };
+
+void Print(){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void Choose(int curr_num){
+    if(curr_num >= n){
+        Print();
+        return;
+    }
+
+    for(int i=0; i<n; i++){
+        if(visited[i] == false){
+            arr[curr_num] = i+1;
+            visited[i] = true;
+            Choose(curr_num+1);
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    Choose(0);
+
+    return 0;
+}
