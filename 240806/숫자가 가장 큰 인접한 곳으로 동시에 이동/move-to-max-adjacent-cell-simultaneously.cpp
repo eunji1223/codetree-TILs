556fd7532ae66196a63636eb73f5cb662d506db4
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-08-06 16:45:28 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
+++ 240806/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define MaxGrid 20
 
 int arr[MaxGrid][MaxGrid];
+int arr_recent[MaxGrid][MaxGrid] = {0, };
 int arr_simulate[MaxGrid][MaxGrid] = {0, };
 
 int dx[4] = { -1, 1, 0, 0};
@@ -12,17 +13,19 @@ int dy[4] = { 0, 0, -1, 1};
 void Simulation(int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(arr_simulate[i][j] == 1){
+            if(arr_recent[i][j] == 1){
                 int max = arr[i][j];
+                int row = i, col = j;
                 for(int k=0; k<4; k++){
-                    int row = i+dx[k];
-                    int col = j+dy[k];
-                    if(arr[row][col] > max){
-                        max = arr[row][col];
-                        arr_simulate[i][j] = 0;
-                        arr_simulate[row][col] += 1;
-                        break;
+                    if(arr[i+dx[k]][j+dy[k]] > max){
+                        max = arr[i+dx[k]][j+dy[k]];
+                        row = i + dx[k];
+                        col = j + dy[k];
                     }
+                }
+                if(max != arr[i][j]){
+                    arr_simulate[i][j] = 0;
+                    arr_simulate[row][col]++;
                 }
             }
         }
@@ -51,11 +54,17 @@ int main() {
         cin >> row >> col;
         row--;
         col--;
+        arr_recent[row][col] = 1;
         arr_simulate[row][col] = 1;
     }
 
-    for(int i=0; i<t; i++){
+    for(int k=0; k<t; k++){
         Simulation(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                arr_recent[i][j] = arr_simulate[i][j];
+            }
+        }
     }
 
     int cnt = 0;
