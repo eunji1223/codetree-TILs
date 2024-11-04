0e33aa039913cd3a850dbb2b08654f61b1bd24e1
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-04 17:53:00 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/그 계절, 그 날/that-season-that-day.cpp
+++ 241104/그 계절, 그 날/that-season-that-day.cpp
@@ -14,21 +14,23 @@ void printSeason(int m, int d){
     if(d > month_days[m-1]){
         cout << -1 << endl;
     }
-    switch(m){
-        case 3: case 4: case 5:
+    else{
+        switch(m){
+            case 3: case 4: case 5:
             cout << "Spring" << endl;
             break;
-        case 6: case 7: case 8:
+            case 6: case 7: case 8:
             cout << "Summer" << endl;
             break;
-        case 9: case 10: case 11:
+            case 9: case 10: case 11:
             cout << "Fall" << endl;
             break;
-        case 1: case 2: case 12:
+            case 1: case 2: case 12:
             cout << "Winter" << endl;
             break;
-        default:
+            default:
             break;
+        }
     }
 }
 
