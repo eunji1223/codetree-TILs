6ce48c977a0be42b44197a82291b78a80c11c24f
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-04 17:51:36 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241104/그 계절, 그 날/that-season-that-day.cpp
+++ 241104/그 계절, 그 날/that-season-that-day.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int month_days[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 bool isLeapYear(int y){
-    if(y%4==0 && (y%100!=0 || y%100==0 && y%400==0)){
+    if((y%4==0 && y%100!=0) || y%100==0 && y%400==0){
         return true;
     }
     return false;
