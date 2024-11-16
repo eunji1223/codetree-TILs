094bfaa2fe403990c5a31ca063162cb06426de65
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-16 17:26:14 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241116/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Number{
+    public:
+        int num, order;
+
+        Number(){}
+
+        Number(int num, int order){
+            this->num = num;
+            this->order = order;
+        }
+};
+
+bool compare(const Number &num1, const Number &num2){
+    return num1.num < num2.num;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    Number numbers[n], numbers_check[n];
+    for(int i=0; i<n; i++){
+        int num;
+        cin >> num;
+        numbers[i] = Number(num, i+1);
+        numbers_check[i] = Number(num, i+1);
+    }
+
+    sort(numbers, numbers+n, compare);
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(numbers_check[i].order==numbers[j].order){
+                cout << j+1 << " ";
+            }
+        }
+    }
+    
+    return 0;
+}
