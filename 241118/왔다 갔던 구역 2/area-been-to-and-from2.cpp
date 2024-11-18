1b106f272893442733c2e5625eae8cbf8bab7244
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-18 21:11:47 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241118/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int map[200] = { 0, };
+
+void paint(int current_loc, int size, char dir){
+    int dir_num;
+    if(dir == 'R'){
+        dir_num = 1;
+    }
+    else if(dir == 'L'){
+        dir_num = -1;
+    }
+    for(int i=0; i<size; i++){
+        map[current_loc+i] += 1;
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int current_loc = 100;
+    for(int i=0; i<n; i++){
+        int size;
+        char dir;
+        cin >> size >> dir;
+        paint(current_loc, size, dir);
+    }
+
+    int cnt = 0;
+    for(int i=0; i<200; i++){
+        if(map[i]>1){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
