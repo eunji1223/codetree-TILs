8957647ff95f686e6dac953c12f15f94bd073aeb
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-20 21:29:16 +0900

    Title: 사각형의 총 넓이 2, Time: 15ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241120/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#define MAX_SIZE 200
+#define OFFSET MAX_SIZE/2
+
+int map[MAX_SIZE][MAX_SIZE] = { 0, };
+
+int GetExtent(int x1, int y1,int x2, int y2){
+    int point = 0;
+    for(int i=x1; i<x2; i++){
+        for(int j=y1; j<y2; j++){
+            if(map[i][j] == 1){
+                point += 1;
+                map[i][j] = 0;
+            }
+        }
+    }
+    return point;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    fill(&map[0][0], &map[MAX_SIZE][MAX_SIZE], 1);
+
+    int x1, y1, x2, y2;
+    int extent = 0;
+    for(int i=0; i<n; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+        extent += GetExtent(x1, y1, x2, y2);
+    }
+    cout << extent; 
+    
+    return 0;
+}
