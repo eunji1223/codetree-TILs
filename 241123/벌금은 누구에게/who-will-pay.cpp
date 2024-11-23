7047ebed4d0a268997a59017f6315c69fe004631
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2024-11-23 12:15:13 +0900

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241123/벌금은 누구에게/who-will-pay.cpp
+++ 241123/벌금은 누구에게/who-will-pay.cpp
@@ -5,15 +5,20 @@ int main() {
     int n, m, k;
     cin >> n >> m >> k;
 
+    int student[n];
+    for(int i=0; i<n; i++){
+        student[i] = k;
+    }
+
     int fund = -1;
-    int student[n] = { k, };
     for(int i=0; i<m; i++){
         int student_num;
         cin >> student_num;
 
-        student[student_num-1]--;
-        if(student[student_num-1] <= 0){
-            fund = student_num;
+        student_num--;
+        student[student_num]--;
+        if(student[student_num] <= 0){
+            fund = student_num+1;
             break;
         }
     }
